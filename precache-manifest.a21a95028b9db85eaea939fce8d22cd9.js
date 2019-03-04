0fd7dbad7ce900ce9b1a64fab9597681fd9856f6
rafaelBadan <rafael.badan@gmail.com>
2019-03-04 16:19:14 -0300

    Updates

---
+++ precache-manifest.a21a95028b9db85eaea939fce8d22cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goreact_redux/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e5b81c6009850936a508",
+    "url": "/goreact_redux/static/js/runtime~main.e5b81c60.js"
+  },
+  {
+    "revision": "7311fcae44d98ec88306",
+    "url": "/goreact_redux/static/js/main.7311fcae.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/goreact_redux/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "7311fcae44d98ec88306",
+    "url": "/goreact_redux/static/css/main.bda3f6df.chunk.css"
+  },
+  {
+    "revision": "3423e1bc915bd4764b88eb07623f67aa",
+    "url": "/goreact_redux/index.html"
+  }
+];
