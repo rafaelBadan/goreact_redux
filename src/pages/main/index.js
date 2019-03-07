a7a1d3699acd599e3f8bdb53b40ddd4abd7d7ea9
rafaelBadan <rafael.badan@gmail.com>
2019-03-07 10:43:08 -0300

    redux-sagas implementation

---
+++ src/pages/main/index.js
@@ -0,0 +1,71 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as FavoriteActions from '../../store/actions/favorites';
+
+class Main extends Component {
+  static propTypes = {
+    addFavorite: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        url: PropTypes.string,
+      }),
+    ),
+  };
+
+  state = {
+    repositoryInput: '',
+  };
+
+  handleAddRepository = (event) => {
+    event.preventDefault();
+
+    this.props.addFavorite();
+  };
+
+  render() {
+    const { repositoryInput } = this.state;
+    const { favorites } = this.props;
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="user/repository"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+        <ul>
+          {favorites.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>{favorite.name}</strong>
+                {favorite.description}
+              </p>
+              <a href={favorite.url}>Visit repository</a>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Main);
